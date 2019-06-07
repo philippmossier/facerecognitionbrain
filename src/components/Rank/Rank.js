693463c1f8ef00e0e18f54f34a1b135e20c145ab
Philipp Mossier <p.mossier.development@gmail.com>
2019-06-07 10:02:57 +0200

    rank1

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -4,7 +4,7 @@ const Rank = () => {
 	return (
 		<div>
 			<div className="white f3">{'Philipp your current rank is...'}</div>
-			<div className="f1 white">{'#2'}</div>
+			<div className="f1 white">{'#1'}</div>
 		</div>
 	);
 };
